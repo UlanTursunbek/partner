f805226643009e8969b1bc82608b7c699f0b1939
UlanTursunbek <ulantursunbekdev@gmail.com>
2023-06-03 02:26:37 +0600

    chore: Tune routing

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { ReactElement } from "react";
+import type { NextPageWithLayout } from "./_app";
+
+const Page: NextPageWithLayout = () => {
+  return <p>hello world</p>;
+};
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <div>{page}</div>;
+};
+
+export default Page;
