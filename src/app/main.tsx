d604a3a88eb26885f2d0ca64c4d50e1f7c50b809
UlanTursunbek <ulantursunbekdev@gmail.com>
2023-08-19 19:54:04 +0600

    chore: Apply chakra provider

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import { App } from './App'
 
@@ -10,8 +11,10 @@ ReactDOM.createRoot(
   document.getElementById('root')!,
 ).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>,
 )
