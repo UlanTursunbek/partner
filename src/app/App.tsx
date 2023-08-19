e139d8748dbe997953504be63de66522f7eb659a
UlanTursunbek <ulantursunbekdev@gmail.com>
2023-08-19 19:53:44 +0600

    feat: Add main layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,23 @@
 import { Route, Routes } from 'react-router-dom'
+import { Main } from 'pages/Main'
 import { NotFound } from 'pages/NotFound'
 import { ShoppingList } from 'pages/ShoppingList'
+import { MainLayout } from 'widgets/Layout'
+
+import { routes } from './routes'
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<div>home</div>} />
-      <Route
-        path="/shopping-list"
-        element={<ShoppingList />}
-      />
+    <MainLayout>
+      <Routes>
+        <Route path={routes.home} element={<Main />} />
+        <Route
+          path={routes.shoppingList}
+          element={<ShoppingList />}
+        />
 
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </MainLayout>
   )
 }
