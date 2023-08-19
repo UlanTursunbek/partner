c798e034ff39515d714fb377d83870632e76dc84
UlanTursunbek <ulantursunbekdev@gmail.com>
2023-08-19 21:37:24 +0600

    feat: Mock dating counter

--- src/widgets/DatingCount.tsx
+++ src/widgets/DatingCount.tsx
@@ -1,9 +1,17 @@
-import { Heading } from '@chakra-ui/react'
+import { chakra, Heading } from '@chakra-ui/react'
 
 export const DatingCount = () => {
   return (
-    <Heading as="h2">
-      (4xl) In love with React & Next
-    </Heading>
+    <chakra.div
+      height={24}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      border="4px solid red"
+      borderRadius={4}
+      bgColor="blue.500"
+    >
+      <Heading as="h2">You are dating for 80 days</Heading>
+    </chakra.div>
   )
 }
